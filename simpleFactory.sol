963fac2df01d248533029d8c165bf591c1d54a29
collins-ruto <collinsruto48@gmail.com>
2022-05-23 22:22:13 +0300

    initial commit

---
+++ simpleFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.0 <0.9.0;
+
+import "./simpleStorage.sol";
+
+contract SimpleFactory {
+
+    SimpleStorage[] public simplestorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simplestorage = new SimpleStorage();
+        simplestorageArray.push(simplestorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNum) public {
+        SimpleStorage(address(simplestorageArray[_simpleStorageIndex])).store(_simpleStorageNum);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return SimpleStorage(address(simplestorageArray[_simpleStorageIndex])).retrieve();
+    }
+}
